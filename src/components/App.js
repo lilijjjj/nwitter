7f6c08ed6a775e37b1eb95cca81f52ef06e1f7a7
lilijjjj <kwonhyunah2@gmail.com>
2021-12-31 17:52:42 +0900

    12/31

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,8 @@
 import React, { useState } from 'react';
 import AppRouter from "./Router";
-import { authService } from "fbase";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  console.log(authService.currentUser);
   return (
   <>
     <AppRouter isLoggedIn={isLoggedIn} />
