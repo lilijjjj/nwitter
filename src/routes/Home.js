dd8d84f12414bde2b3c77be0b4d36273287166c7
lilijjjj <kwonhyunah2@gmail.com>
2022-03-23 15:34:11 +0900

    finish

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -24,6 +24,7 @@ const Home = ({userObj}) => {
       }, []);
 
     return (
+      
         <div className="container">
           <NweetFactory userObj={userObj}/>
           <div style={{ marginTop: 30 }}>
